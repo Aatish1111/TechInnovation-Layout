a7a760e1475d7d439a439dd3a8c0875db0b2d66d
Aatish1111 <aatishkumarmoharana@gmail.com>
2023-12-21 14:13:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Timesheet from './components/Timesheet';
+import TaskManager from './components/TaskManager';
+import LeaveManagement from './components/LeaveManagement';
+import Payroll from './components/Payroll';
+import Integrations from './components/Integrations';
+import AdminToolbar from './components/AdminToolbar';
+import Middle from './components/Middle';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        {/* Vertical Left Div */}
+        <div className="col-md-3">
+          <div className="mb-3">
+            <Timesheet />
+          </div>
+          <div className="mb-3">
+            <TaskManager />
+          </div>
+          <div className="mb-3">
+            <LeaveManagement />
+          </div>
+          <div className="mb-3">
+            <Payroll />
+          </div>
+          <div className="mb-3">
+            <Integrations />
+          </div>
+          <div className="mb-3">
+            <AdminToolbar />
+          </div>
+        </div>
+
+        {/* Middle Div */}
+        <div className="col-md-6">
+          {/* Your content in the middle div */}
+          <Middle />
+        </div>
+
+        {/* Vertical Right Div */}
+        <div className="col-md-3">
+          {/* Content for the vertical right div */}
+        </div>
+      </div>
+
+      {/* Horizontal Top Div */}
+      <div className="row mt-3">
+        <div className="col">
+          {/* Content for the horizontal top div */}
+        </div>
+      </div>
+
+      {/* Horizontal Bottom Div */}
+      <div className="row mt-3">
+        <div className="col">
+          {/* Content for the horizontal bottom div */}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
