b2c10700dcdad3930d494605e21d3c38f1b5e99c
Aatish1111 <aatishkumarmoharana@gmail.com>
2023-12-26 01:16:16 +0530

    Structuring Commit

--- src/App.js
+++ src/App.js
@@ -9,65 +9,71 @@ import Payroll from './components/Payroll';
 import Integrations from './components/Integrations';
 import AdminToolbar from './components/AdminToolbar';
 import Middle from './components/Middle';
+import Footer from './components/Footer'; // Assuming you have a Footer component
 
 const App = () => {
+  const [selectedComponent, setSelectedComponent] = useState(null);
 
-    const [selectedComponent, setSelectedComponent] = useState(null);
-  
-    const handleComponentClick = (componentName) => {
-      setSelectedComponent(componentName);
-    };
+  const handleComponentClick = (componentName) => {
+    setSelectedComponent(componentName);
+  };
+
   return (
-    <div className="container-fluid mt-3">
-      <div className="row ">
-        {/* Vertical Left Div */}
+    <div className="container-fluid">
+      {/* Top Section */}
+      <div className="row">
+        <div className="col">
+          {/* Content for the top section */}
+          <h1>The Top Component</h1>
+        </div>
+      </div>
+
+      {/* Middle Section */}
+      <div className="row">
+        {/* Left Div */}
         <div className="col-md-3">
           <div className="mb-3">
-          <Timesheet onClick={() => handleComponentClick('Timesheet')} />
+            <Timesheet onClick={() => handleComponentClick('Timesheet')} />
           </div>
           <div className="mb-3">
-          <TaskManager onClick={() => handleComponentClick('TaskManager')} />
+            <TaskManager onClick={() => handleComponentClick('TaskManager')} />
           </div>
           <div className="mb-3">
-            <LeaveManagement onClick={() => handleComponentClick('LeaveManagement')}/>
+            <LeaveManagement onClick={() => handleComponentClick('LeaveManagement')} />
           </div>
           <div className="mb-3">
-            <Payroll onClick={() => handleComponentClick('Payroll')}/>
+            <Payroll onClick={() => handleComponentClick('Payroll')} />
           </div>
           <div className="mb-3">
-            <Integrations onClick={() => handleComponentClick('Integrations')}/>
+            <Integrations onClick={() => handleComponentClick('Integrations')} />
           </div>
           <div className="mb-3">
-            <AdminToolbar onClick={() => handleComponentClick('AdminToolbar')}/>
+            <AdminToolbar onClick={() => handleComponentClick('AdminToolbar')} />
           </div>
         </div>
 
         {/* Middle Div */}
-        <div className="col-md-6 mt-5 w-40" >
-       <div>
-       You had clicked on <span className='text-danger'><Middle selectedComponent={selectedComponent} /> </span>component
-       </div>
+        <div className="col-md-6">
+          <div>
+            You had clicked on <span className='text-danger'><Middle selectedComponent={selectedComponent} /></span> component
+          </div>
         </div>
 
-        {/* Vertical Right Div */}
+        {/* Right Div */}
         <div className="col-md-3">
-          {/* Content for the vertical right div */}
+          {/* Content for the right div */}
         </div>
       </div>
 
-      {/* Horizontal Top Div */}
-      <div className="row mt-3">
+      {/* Bottom Section */}
+      <div className="row">
         <div className="col">
-          {/* Content for the horizontal top div */}
+          {/* Content for the bottom section */}
         </div>
       </div>
 
-      {/* Horizontal Bottom Div */}
-      <div className="row mt-3">
-        <div className="col">
-          {/* Content for the horizontal bottom div */}
-        </div>
-      </div>
+      {/* Footer Section */}
+      <Footer />
     </div>
   );
 };
